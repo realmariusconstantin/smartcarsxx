d90a9ac8d362d42274d750cc03605804e5a617f2
Marius Constantin <68522001+realmariusconstantin@users.noreply.github.com>
2024-11-24 13:51:04 +0000

    added class Vehicle

--- src/Data/Vehicle.java
+++ src/Data/Vehicle.java
@@ -3,14 +3,14 @@ package Data;
 import java.time.LocalDate;
 
 public abstract class Vehicle {
-    private int id;
-    private String model;
-    private String regNo;
-    private String manufacturer;
-    private LocalDate regDate;
-    private String location;
+    private int id; //Unique identifier
+    private String model; //Model of the vehicle
+    private String regNo; //Registration number
+    private String manufacturer; //Manufacturer of the vehicle
+    private LocalDate regDate; //Registration date
+    private String location; //Location of the vehicle
 
-    public Vehicle(int id, String model, String regNo, String manufacturer, LocalDate regDate, String location) {
+    public Vehicle(int id, String model, String regNo, String manufacturer, LocalDate regDate, String location) { //Constructor
         this.id = id;
         this.model = model;
         this.regNo = regNo;
@@ -21,5 +21,12 @@ public abstract class Vehicle {
 
 
     //Different for each type of vehicle
-    public abstract double calculateRate(int hours, int km);
+    public abstract double calculateRate(int hours, int km); //This calculates the base rate based on the number of hours and the distance travelled.
+
+    public String toString(){
+        return "vehicle ID: " + id
+                + "\nRegistration: " + regNo + " registered on " + regDate
+                + "\nModel: " + manufacturer + " " + model
+                + "\nLocation " + location;
+    }
 }
