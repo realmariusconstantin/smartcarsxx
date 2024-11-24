67e972481c62d84baff89253b0d4f918873f205f
Marius Constantin <68522001+realmariusconstantin@users.noreply.github.com>
2024-11-24 14:23:58 +0000

    Added Person Class

---
+++ src/Data/Person.java
@@ -0,0 +1,15 @@
+package Data;
+
+public abstract class Person {
+    private String firstname; // First name of the person
+    private String lastname; // Last name of the person
+    private String email; // Email address of the person
+    private String address; // Address of the person
+
+    public Person(String firstname, String lastname, String email, String address) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.address = address;
+    }
+}
