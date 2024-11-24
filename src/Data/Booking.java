6a3c4709fb4cb7131b3d8ce38e56ecd373e00a96
Marius Constantin <68522001+realmariusconstantin@users.noreply.github.com>
2024-11-24 14:34:52 +0000

    Booking Class Added

---
+++ src/Data/Booking.java
@@ -0,0 +1,26 @@
+package Data;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+// Booking class
+public class Booking {
+    private int bookingId; // Booking number
+    private int vehicleId; // Vehicle number
+    private int custNo; // Customer number
+    private LocalDate pickUpDate; // Pick up date
+    private LocalTime pickUpTime; // Pick up time
+    private LocalDate returnDate; // Return date
+    private LocalTime returnTime; // Return time
+    private String pickUpLocation; // Pick up location
+
+    public Booking(int bookingId, int vehicleId, int custNo, LocalDate pickUpDate, LocalTime pickUpTime, LocalDate returnDate, LocalTime returnTime, String pickUpLocation) {
+        this.bookingId = bookingId;
+        this.vehicleId = vehicleId;
+        this.custNo = custNo;
+        this.pickUpDate = pickUpDate;
+        this.pickUpTime = pickUpTime;
+        this.returnDate = returnDate;
+        this.returnTime = returnTime;
+        this.pickUpLocation = pickUpLocation;
+    }
+}
