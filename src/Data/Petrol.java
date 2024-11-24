6da0e537fbd09323742d67ee6781aef926f9b8ab
Marius Constantin <68522001+realmariusconstantin@users.noreply.github.com>
2024-11-24 14:14:53 +0000

    Petrol class added

---
+++ src/Data/Petrol.java
@@ -0,0 +1,14 @@
+package Data;
+import java.time.LocalDate;
+
+// Petrol class is an abstract class that extends Vehicle class
+public abstract class Petrol extends Vehicle{
+    private double engine; // Engine size in litres
+    private int co2; // CO2 emissions in g/km
+
+    public Petrol(int id, String model, String regNo, String manufacturer, LocalDate regDate, String location, double engine, int co2) {
+        super(id, model, regNo, manufacturer, regDate, location);
+        this.engine = engine;
+        this.co2 = co2;
+    }
+}
